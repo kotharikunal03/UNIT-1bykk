37c01ebd5cfd9d6aeb1905a04a1a695a4248cf75
kotharikunal03 <kotharikunal03@gmail.com>
2026-03-10 22:40:08 +0530

    uploading my script

---
+++ list_operations.py
@@ -0,0 +1,19 @@
+# Create list
+my_list = [5, 2, 9, 1, 7]
+
+# Access
+print("First element:", my_list[0])
+
+# Add element
+my_list.append(10)
+
+# Remove element
+my_list.remove(2)
+
+# Sort list
+my_list.sort()
+
+# Reverse list
+my_list.reverse()
+
+print("Final list:", my_list)
